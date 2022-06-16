f95efefe55bdffdbacf2ef59d2d247f6a4275c56
Federico Mazzini <federicodmazzini@gmail.com>
2022-06-16 16:20:23 +0200

    hello

---
+++ Shared/TemplumApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplumApp.swift
+//  Shared
+//
+//  Created by federico mazzini on 16/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TemplumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
